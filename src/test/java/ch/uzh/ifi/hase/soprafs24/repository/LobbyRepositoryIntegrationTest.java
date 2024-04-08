2aa2702ff0f644ddc2c98b3c8601f5a59f0d28d4
frippstein <fabio.rippstein@bluewin.ch>
2024-04-08 18:06:55 +0200

    Added test for LobbyRepository ( issue #41 )

--- src/test/java/ch/uzh/ifi/hase/soprafs24/repository/LobbyRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/repository/LobbyRepositoryIntegrationTest.java
@@ -1,8 +1,18 @@
 package ch.uzh.ifi.hase.soprafs24.repository;
 
+import ch.uzh.ifi.hase.soprafs24.entity.Lobby;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@DataJpaTest
 public class LobbyRepositoryIntegrationTest {
 
     @Autowired
@@ -11,4 +21,29 @@ public class LobbyRepositoryIntegrationTest {
     @Autowired
     private LobbyRepository lobbyRepository;
 
+    @Test
+    public void findLobbyByUserId_success() {
+        // given
+        Lobby lobby = new Lobby();
+        lobby.setLobbySize(4);
+        lobby.setNumberOfPlayers(2);
+        lobby.setGameStarted(false);
+        List<Long> players = new ArrayList<Long>();
+        players.add(1L);
+        players.add(2L);
+        lobby.setUsersInLobby(players);
+
+        entityManager.persist(lobby);
+        entityManager.flush();
+
+        // when
+        Lobby found = lobbyRepository.findLobbyByUserId(1L).orElseThrow(() -> new RuntimeException("Lobby not found"));
+
+        // then
+        assertEquals(found.getId(), lobby.getId());
+        assertEquals(found.getUsersInLobby(), lobby.getUsersInLobby());
+        assertEquals(found.getLobbySize(), lobby.getLobbySize());
+        assertEquals(found.getNumberOfPlayers(), lobby.getNumberOfPlayers());
+        assertEquals(found.getGameStarted(), lobby.getGameStarted());
+    }
 }
