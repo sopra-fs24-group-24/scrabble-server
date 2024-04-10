58687935b2e9c80ada7f5e71bc598a511aafa4f1
mikech75 <michael.barry.ch@gmail.com>
2024-04-10 22:09:24 +0200

    Get Game Information #48

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/GameController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/GameController.java
@@ -1,5 +1,6 @@
 package ch.uzh.ifi.hase.soprafs24.controller;
 
+import ch.uzh.ifi.hase.soprafs24.entity.Game;
 import ch.uzh.ifi.hase.soprafs24.service.GameService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -9,12 +10,23 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-public class GameController {
+public class GameController 
+{
 
     private final GameService gameService;
 
-    GameController(GameService gameService) {
+    GameController(GameService gameService) 
+    {
         this.gameService = gameService;
     }
 
+    @RequestMapping("/games/{gameId}")
+    @ResponseBody
+    public Game getGame(@PathVariable("gameId") Long gameId,@RequestParam(required=false) String token) 
+    {
+        // TODO: Need to check token first
+        Game requestedGame=gameService.getGameParams(gameId);
+        return requestedGame;
+    }
+        
 }
