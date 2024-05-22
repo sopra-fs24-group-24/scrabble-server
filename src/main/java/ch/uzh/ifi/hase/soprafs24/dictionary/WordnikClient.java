41fc34bd903e551d0ce16182abe94efd83774c63
flolanz <florian.lanz@uzh.ch>
2024-05-22 18:22:22 +0200

    API fix

--- src/main/java/ch/uzh/ifi/hase/soprafs24/dictionary/WordnikClient.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/dictionary/WordnikClient.java
@@ -14,19 +14,17 @@ import java.net.http.HttpResponse;
 @Service("dictionary")
 public class WordnikClient implements Dictionary{
 
-    @Value("${wordnik.api.key}")
-    private String API_KEY;
     private static final String BASE_URL = "https://api.wordnik.com/v4/word.json/";
     private static final HttpClient client = HttpClient.newHttpClient();
     private static final HttpResponse.BodyHandler<String> bodyHandler = HttpResponse.BodyHandlers.ofString();
 
     public HttpResponse<String> getScrabbleScore(String word) {
-        URI uri = URI.create(BASE_URL + word.toLowerCase() + "/scrabbleScore?api_key=" + API_KEY);
+        URI uri = URI.create(BASE_URL + word.toLowerCase() + "/scrabbleScore?api_key=" + "5bnrdw90wu3xdr5kjx69iq53f4m2gi2ua1inzs9yol07vfgc8");
         return sendGetRequest(uri);
     }
 
     public HttpResponse<String> getWordDefinition(String word) {
-        URI uri = URI.create(BASE_URL + word.toLowerCase() + "/definitions?limit=1&sourceDictionaries=wiktionary&api_key=" + API_KEY);
+        URI uri = URI.create(BASE_URL + word.toLowerCase() + "/definitions?limit=1&sourceDictionaries=wiktionary&api_key=" + "5bnrdw90wu3xdr5kjx69iq53f4m2gi2ua1inzs9yol07vfgc8");
         return sendGetRequest(uri);
     }
 
